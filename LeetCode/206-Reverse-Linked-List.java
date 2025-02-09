4fa12fb3e9875656baa07009310fa5f4d5e374d0
Devendra Sahni <107319947+DevSahni@users.noreply.github.com>
2025-02-09 16:27:36 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 28.61%] - LeetPush

---
+++ LeetCode/206-Reverse-Linked-List.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;  // Store the next node
+            current.next = prev;  // Reverse the current node's pointer
+            prev = current;       // Move prev one step forward
+            current = next;       // Move current one step forward
+        }
+
+        return prev;  // Prev will be the new head after reversing
+    }
+
+    // Helper method to print the linked list
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
